44ce611986ddc00f5a5cd2c186ecdfc92560406c
Kevin Lin <31615974+cj-lin@users.noreply.github.com>
2022-09-29 17:09:56 +0000

    init project

---
+++ tmd67_be/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for tmd67_be project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tmd67_be.settings")
+
+application = get_asgi_application()
